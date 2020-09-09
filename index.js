f01c64b7e9394a29c845d89a736c2d8243ead37e
Michael Naquin <mnaquin1@yahoo.com>
2020-09-09 18:19:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
 var array = []
-  for (let i = 1; i < musicians.length;i++){
+  for (let i = 1; i < musicians.length; i++){
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array
 }
+
+function johnLennonFacts
