e92a027b72aa27733aea5493d58bf5db1c1ecd3e
Michael Naquin <mnaquin1@yahoo.com>
2020-09-09 18:35:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+var array = []
+  for (let i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(fact){
+var lennonArray = []
+let i = 0
+while (i < fact.length){
+  lennonArray.push(fact[i] + '!!!')
+  i++
+}
+return lennonArray
+}
+
+
+function iLoveTheBeatles(n){
+ var array = []
+do{
+  array.push("I love the Beatles!")
+  n++
+} while (n < 15)
+return array
+ }
